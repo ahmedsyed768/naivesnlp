5d047aaa903938c19e8f270c5ecf83efaab42b9a
ahmedsyed768 <talk2syedahmed@gmail.com>
2024-06-08 10:36:13 +0400

    Delete nltk_setup.py

--- nltk_setup.py
+++
@@ -1,9 +0,0 @@
-Python 3.8.7 (v3.8.7:6503f05dd5, Dec 21 2020, 12:45:15) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-import nltk
-
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
