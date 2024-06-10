22a24019fa096404b47f10a22de408a0708935d8
ahmedsyed768 <talk2syedahmed@gmail.com>
2024-06-10 23:05:29 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,7 @@ if uploaded_file:
         st.subheader("Naive Bayes Classifier")
         reviews = df.columns.values.flatten().tolist()
         #labels = [1 if sentiment['compound'] >= 0.65 else 0 for column in df.columns for sentiment in sentiments[column]]
-        labels = [1 if sentiment >= 0.65 else 0 for column in sentiments for sentiment in sentiments[column]]
+        labels = [1 if sentiment >= 0.65 else 0 for df.column in sentiments for sentiment in sentiments[column]]
         for column in df.columns:
             print(f"Column: {column}, Sentiments: {sentiments[column]}")
         pipeline = analyzer.train_classifier(reviews, labels)
