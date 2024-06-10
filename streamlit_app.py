cfe97a86ea768dbad4444899e945e9dc6a5dfdd2
ahmedsyed768 <talk2syedahmed@gmail.com>
2024-06-10 23:22:57 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,7 +143,7 @@ if uploaded_file:
         #reviews = df['teaching'].dropna().astype(str).tolist()
         #labels = [1 if sentiment['compound'] >= 0.65 else 0 for column in df.columns for sentiment in sentiments[column]]
         #labels = [1 if sentiment >= 0.65 else 0 for df.column in sentiments for sentiment in sentiments[column]]
-        labels = [1 if sentiment['compound'] >= 0.65 else 0 for df.column in sentiments for sentiment in sentiments[df.column]]
+        labels = [1 if sentiment >= 0.65 else 0 for sentiment in teaching_sentiments]
         for column in df.columns:
             print(f"Column: {column}, Sentiments: {sentiments[column]}")
         pipeline = analyzer.train_classifier(reviews, labels)
